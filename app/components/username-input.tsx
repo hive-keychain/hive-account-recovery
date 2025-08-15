f387de36035ace9a570f6e83974254b8747ec2ff
carlodizon03 <carlodizon03@gmail.com>
2025-08-15 16:25:28 +0800

    username on change callback

--- app/components/username-input.tsx
+++ app/components/username-input.tsx
@@ -1,5 +1,15 @@
 import { Form } from "react-bootstrap";
 
-export default function UsernameInput() {
-  return <Form.Control type="text" placeholder="Enter username" />;
+export default function UsernameInput({
+  onChangeCallback,
+}: {
+  onChangeCallback: Function;
+}) {
+  return (
+    <Form.Control
+      type="text"
+      placeholder="Enter username"
+      onChange={(e) => onChangeCallback(e.target.value)}
+    />
+  );
 }
