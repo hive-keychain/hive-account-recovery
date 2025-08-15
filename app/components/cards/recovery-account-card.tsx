6ca01c3250cdf0f44472ffe8b08481398c255e9a
carlodizon03 <carlodizon03@gmail.com>
2025-08-16 00:23:57 +0800

    recovery account card placeholder

--- app/components/cards/recovery-account-card.tsx
+++ app/components/cards/recovery-account-card.tsx
@@ -1,15 +1,19 @@
 import { Card } from "react-bootstrap";
+import type { RecoveryAccountRow } from "~/utils/spreadsheet-utils/speadsheet-utils";
 
 export default function RecoveryAccountCard({
   recoveryAccount,
 }: {
-  recoveryAccount: string;
+  recoveryAccount: RecoveryAccountRow;
 }) {
   return (
     <Card>
       <Card.Body>
         <Card.Title>Recovery Account</Card.Title>
-        <Card.Text>{recoveryAccount}</Card.Text>
+        <Card.Text>{recoveryAccount.username}</Card.Text>
+        <Card.Text>{recoveryAccount.data}</Card.Text>
+        <Card.Text>{recoveryAccount.amount}</Card.Text>
+        <Card.Text>{recoveryAccount.expiration}</Card.Text>
       </Card.Body>
     </Card>
   );
