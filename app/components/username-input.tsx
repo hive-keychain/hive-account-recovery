e6f9f93d5ee81f438bb65978d23dbf2bddae9e3c
carlodizon03 <carlodizon03@gmail.com>
2025-08-16 00:23:22 +0800

    added username onEnterCallback

--- app/components/username-input.tsx
+++ app/components/username-input.tsx
@@ -2,14 +2,23 @@ import { Form } from "react-bootstrap";
 
 export default function UsernameInput({
   onChangeCallback,
+  onEnterCallback,
 }: {
   onChangeCallback: Function;
+  onEnterCallback?: Function;
 }) {
+  const handleKeyDown = (e: React.KeyboardEvent<HTMLInputElement>) => {
+    if (e.key === "Enter" && onEnterCallback) {
+      onEnterCallback();
+    }
+  };
+
   return (
     <Form.Control
       type="text"
       placeholder="Enter username"
       onChange={(e) => onChangeCallback(e.target.value)}
+      onKeyDown={handleKeyDown}
     />
   );
 }
