a32918b451f32703d30de5d93ea4daa5078f76c3
carlodizon03 <carlodizon03@gmail.com>
2025-08-16 16:27:51 +0800

    display only the data, tx must happen on another modal

--- app/components/modals/account-update-modal.tsx
+++ app/components/modals/account-update-modal.tsx
@@ -4,87 +4,71 @@ import { AccountUtils } from "~/hive-utils/account-utils";
 import { useEffect, useState } from "react";
 import { AuthorityCard } from "../cards/authority-card";
 import { Spinner } from "react-bootstrap";
+import BroadcastUpdateModal from "./broadcast-update-modal";
 
 export interface AccountUpdateModalProps {
   username: string;
-  newAuthorities: IAuthorities;
-  method: string;
+  updatedAccountData: IAuthorities;
+  show: boolean;
   onCancel: () => void;
+  onUpdate: () => void;
 }
 
 export default function AccountUpdateModal({
   username,
-  newAuthorities,
-  method,
+  updatedAccountData,
+  show,
   onCancel,
+  onUpdate,
 }: AccountUpdateModalProps) {
-  const [updatedAccountData, setUpdatedAccountData] =
-    useState<IAuthorities | null>(null);
-
-  useEffect(() => {
-    AccountUtils.getUpdatedAccountData(username, newAuthorities).then(
-      (data) => {
-        console.log({ data });
-        setUpdatedAccountData(data);
-      }
-    );
-  }, [username, newAuthorities]);
   return (
-    <Modal size="lg" centered show={true} onHide={() => {}}>
-      <Modal.Header>
-        <Modal.Title>Account Update</Modal.Title>
-      </Modal.Header>
-      <Modal.Body>
-        {updatedAccountData ? (
-          <div>
-            <p>
-              Are you sure you want to update the account <b>@{username}</b>{" "}
-              with the following authorities?
-            </p>
+    <>
+      <Modal size="lg" centered show={show} onHide={onCancel}>
+        <Modal.Header>
+          <Modal.Title>Account Update</Modal.Title>
+        </Modal.Header>
+        <Modal.Body>
+          {updatedAccountData ? (
             <div>
-              <AuthorityCard
-                name="Owner"
-                authority={updatedAccountData.owner}
-              />
-              <AuthorityCard
-                name="Active"
-                authority={updatedAccountData.active}
-              />
-              <AuthorityCard
-                name="Posting"
-                authority={updatedAccountData.posting}
-              />
-              <AuthorityCard
-                name="Memo Key"
-                memoKey={updatedAccountData.memo_key}
-              />
-             
+              <p>
+                Are you sure you want to update the account <b>@{username}</b>{" "}
+                with the following authorities?
+              </p>
+              <div>
+                <AuthorityCard
+                  name="Owner"
+                  authority={updatedAccountData.owner}
+                />
+                <AuthorityCard
+                  name="Active"
+                  authority={updatedAccountData.active}
+                />
+                <AuthorityCard
+                  name="Posting"
+                  authority={updatedAccountData.posting}
+                />
+                <AuthorityCard
+                  name="Memo Key"
+                  memoKey={updatedAccountData.memo_key}
+                />
+              </div>
             </div>
-          </div>
-        ) : (
-          <div className="d-flex justify-content-center align-items-center">
-            <Spinner />
-            <p>Fetching account data...</p>
-          </div>
-        )}
-      </Modal.Body>
-      <Modal.Footer>
-        <Button variant="outline-secondary" onClick={onCancel}>
-          Cancel
-        </Button>
-        <Button
-          variant="outline-primary"
-          onClick={() => {
-            AccountUtils.accountUpdateBroadcast(
-              username,
-              newAuthorities,
-              method
-            );
-          }}
-        >
-          Update
-        </Button>
-      </Modal.Footer>
-    </Modal>
+          ) : (
+            <div className="d-flex justify-content-center align-items-center">
+              <Spinner />
+              <p>Fetching account data...</p>
+            </div>
+          )}
+        </Modal.Body>
+        <Modal.Footer>
+          <Button variant="outline-secondary" onClick={onCancel}>
+            Cancel
+          </Button>
+          <Button variant="outline-primary" onClick={onUpdate}>
+            Update
+          </Button>
+        </Modal.Footer>
+      </Modal>
+    </>
   );
 }
